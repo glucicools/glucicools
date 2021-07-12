f38b51acce6475be7d1150d82ac63c6f32f8b33f
o-marvel <osemekemrvlls@gmail.com>
2021-07-12 21:43:05 +0100

    commit assign

---
+++ week 6.kt
@@ -0,0 +1,12 @@
+
+// fun that takes two number
+fun inlove ( timmy : Int , sarah : Int ): Boolean{
+    if((timmy %2 == 0).xor(sarah%2 == 0)) return true
+    return false
+
+}
+
+fun main (){
+ println(inlove(22,11))
+
+}
