762d0409b9f532e68ae83c6f4ba7a59ee9f598b2
o-marvel <osemekemrvlls@gmail.com>
2021-07-13 17:17:44 +0100

    fall in love

--- week 6.kt
+++ week 6.kt
@@ -1,5 +1,4 @@
 
-// fun that takes two number
 fun inlove ( timmy : Int , sarah : Int ): Boolean{
     if((timmy %2 == 0).xor(sarah%2 == 0)) return true
     return false
--- weekfive.kt
+++
@@ -1,8 +0,0 @@
- fun main(){
-     var str = "life is more than hardship"
-    println(remVowels(str))
- }
-  fun  remVowels ( f : String) : String{
-      var vowels = setOf('a', 'e', 'i','o', 'u')
-      return f.filter { it !in vowels }
-  }
